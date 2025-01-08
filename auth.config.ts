d1d2f807f62ed5e45481a96499dcfbe8afc2d6e9
J164 <edleyjacob@gmail.com>
2025-01-08 11:53:30 -0600

    fix jwt strategy

--- auth.config.ts
+++ auth.config.ts
@@ -8,6 +8,17 @@ export const authConfig = {
     callbacks: {
         authorized({ auth, request: { nextUrl } }) {
             return !nextUrl.pathname.startsWith("/dashboard") || !!auth;
+        },
+        jwt({ token, user }) {
+            if (user) {
+                // User is available during sign-in
+                token.id = user.id;
+            }
+            return token;
+        },
+        session({ session, token }) {
+            session.user.id = token.id as string;
+            return session;
         }
     },
     providers: [Google]
