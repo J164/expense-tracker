7acaa97c73a693ae6646de0d19a7a87d28f07ec3
J164 <edleyjacob@gmail.com>
2025-01-23 20:22:26 -0600

    style login button

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,7 +10,20 @@ export default {
         extend: {
             colors: {
                 background: "var(--background)",
-                foreground: "var(--foreground)"
+                foreground: "var(--foreground)",
+                // https://www.paulie.dev/posts/2023/11/a-set-of-sign-in-with-google-buttons-made-with-tailwind/
+                google: {
+                    "text-gray": "#3c4043",
+                    "button-blue": "#1a73e8",
+                    "button-blue-hover": "#5195ee",
+                    "button-dark": "#202124",
+                    "button-dark-hover": "#555658",
+                    "button-border-light": "#dadce0",
+                    "logo-blue": "#4285f4",
+                    "logo-green": "#34a853",
+                    "logo-yellow": "#fbbc05",
+                    "logo-red": "#ea4335"
+                }
             }
         }
     },
