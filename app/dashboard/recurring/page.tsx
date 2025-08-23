19c0a824af336830cb64e163d7593a1e0a43ded9
Jacob <edleyjacob@gmail.com>
2025-08-22 23:17:30 -0500

    adjust styles

--- app/dashboard/recurring/page.tsx
+++ app/dashboard/recurring/page.tsx
@@ -1,3 +1,4 @@
+import { lusitana } from "@/app/ui/dashboard/fonts";
 import Pagination from "@/app/ui/transactions/pagination";
 import Search from "@/app/ui/search";
 import RecurringTransactionsTable from "@/app/ui/recurring/table";
@@ -26,7 +27,9 @@ export default async function Page(props: {
     return (
         <div className="w-full">
             <div className="flex w-full items-center justify-between">
-                <h1 className="text-2xl">Recurring Transactions</h1>
+                <h1 className={`${lusitana.className} text-2xl`}>
+                    Recurring Transactions
+                </h1>
             </div>
             <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
                 <Search placeholder="Search recurring transactions..." />
--- app/ui/profile/profile.tsx
+++ app/ui/profile/profile.tsx
@@ -7,39 +7,38 @@ import { updateProfile } from "@/app/lib/actions/profile";
 
 export default function Profile({ profile }: { profile: FormatProfile }) {
     return (
-        <form action={updateProfile}>
-            <div className="rounded-md bg-gray-50 p-4 md:p-6">
-                <div className="mb-4">
-                    <label
-                        htmlFor="amount"
-                        className="mb-2 block text-sm font-medium"
-                    >
-                        Enter your monthly budget
-                    </label>
-                    <div className="relative mt-2 rounded-md">
-                        <div className="relative">
-                            <input
-                                id="budget"
-                                name="budget"
-                                type="number"
-                                step="0.01"
-                                placeholder="Enter USD amount"
-                                defaultValue={profile.monthly_budget}
-                                className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 pr-2 text-sm outline-2 placeholder:text-gray-500"
-                                required
-                            />
-                            <CurrencyDollarIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+        <div className="rounded-md border p-4 md:p-6">
+            <h2 className="text-xl font-semibold mb-4">Budget Settings</h2>
+            <form action={updateProfile}>
+                <div className="rounded-md bg-gray-50 p-4 md:p-6">
+                    <div className="mb-4">
+                        <label
+                            htmlFor="amount"
+                            className="mb-2 block text-sm font-medium"
+                        >
+                            Enter your monthly budget
+                        </label>
+                        <div className="relative mt-2 rounded-md">
+                            <div className="relative">
+                                <input
+                                    id="budget"
+                                    name="budget"
+                                    type="number"
+                                    step="0.01"
+                                    placeholder="Enter USD amount"
+                                    defaultValue={profile.monthly_budget}
+                                    className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 pr-2 text-sm outline-2 placeholder:text-gray-500"
+                                    required
+                                />
+                                <CurrencyDollarIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+                            </div>
                         </div>
-                        <p className="mt-2 text-xs text-gray-500">
-                            Note: this will not affect the current month&apos;s
-                            budget
-                        </p>
                     </div>
                 </div>
-            </div>
-            <div className="mt-6 flex justify-start gap-4">
-                <Button type="submit">Save Changes</Button>
-            </div>
-        </form>
+                <div className="mt-6 flex justify-start gap-4">
+                    <Button type="submit">Save Changes</Button>
+                </div>
+            </form>
+        </div>
     );
 }
